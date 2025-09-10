3a1e1d3661a2b89c19459d21127241e7f7c9a56d
EdgarCamberos1894 <edgarcamberos18@gmail.com>
2025-09-09 20:02:15 -0700

    fix(migration): update image insert statements in Flyway scripts

--- backend/rentify/src/main/resources/db/migration/mysql/V17__Add_data_multimedia_column.sql
+++ backend/rentify/src/main/resources/db/migration/mysql/V17__Add_data_multimedia_column.sql
@@ -1,15 +1,16 @@
 INSERT INTO properties_multimedia (type, url, property_id )
 VALUES
-    ('IMAGE', 'https://res.cloudinary.com/dabgkc4zi/image/upload/v1730331363/Cloudinary-React/gdaarakazxgmzidqbhpm.png',1),
-    ('IMAGE', 'https://res.cloudinary.com/dabgkc4zi/image/upload/v1730331362/Cloudinary-React/afhdlfnjavgddsnldmpd.png',2),
-    ('IMAGE', 'https://res.cloudinary.com/dabgkc4zi/image/upload/v1730331361/Cloudinary-React/hmsdqegm7eoejgndrlxz.png',3),
-    ('IMAGE', 'https://res.cloudinary.com/dabgkc4zi/image/upload/v1730331361/Cloudinary-React/ycjaffo21yelcp3gyh9x.png',4),
-    ('IMAGE', 'https://res.cloudinary.com/dabgkc4zi/image/upload/v1730331359/Cloudinary-React/qnmsuhurvmjfrbpkpvn9.png',5),
-    ('IMAGE', 'https://res.cloudinary.com/dabgkc4zi/image/upload/v1730331359/Cloudinary-React/whxpqdorus51wo73t5xo.png',6),
-    ('IMAGE', 'https://res.cloudinary.com/dabgkc4zi/image/upload/v1730331358/Cloudinary-React/qz7iulqk4jl8wdunkabs.png',7),
-    ('IMAGE', 'https://res.cloudinary.com/dabgkc4zi/image/upload/v1730331356/Cloudinary-React/jt42gbgyvznp6fkpnv2o.png',8),
-    ('IMAGE', 'https://res.cloudinary.com/dabgkc4zi/image/upload/v1730331356/Cloudinary-React/aky6fjdqahgjj95jii8q.png',9),
-    ('IMAGE', 'https://res.cloudinary.com/dabgkc4zi/image/upload/v1730331355/Cloudinary-React/mxttmm8kdzmvxnc7y8mk.png',10),
-    ('IMAGE', 'https://res.cloudinary.com/dabgkc4zi/image/upload/v1730331354/Cloudinary-React/u3gdsmghqjxh5n2mzsfk.png',11),
-    ('IMAGE', 'https://res.cloudinary.com/dabgkc4zi/image/upload/v1730331354/Cloudinary-React/ozhfie898qj7dpuz8ppe.png',12)
-    ;
+    ('IMAGE', 'https://res.cloudinary.com/dggmk0peo/image/upload/v1757241176/remate_de_ocasion_barato_luup_8650028722601195267_qblutn.png',1),
+    ('IMAGE', 'https://res.cloudinary.com/dggmk0peo/image/upload/v1757241430/casa_potencial_desarrollo_hvsh5z.png',2),
+    ('IMAGE', 'https://res.cloudinary.com/dggmk0peo/image/upload/v1757241014/eb_l8dlg5.png',3),
+    ('IMAGE', 'https://res.cloudinary.com/dggmk0peo/image/upload/v1757241032/mobiliario-de-oficina_dvjqeb.png',4),
+    ('IMAGE', 'https://res.cloudinary.com/dggmk0peo/image/upload/v1757241027/Main_image_entfuj.png',5),
+    ('IMAGE', 'https://res.cloudinary.com/dggmk0peo/image/upload/v1757241023/IMG-20210602-WA0033-608x570_y1sdqi.png',6),
+    ('IMAGE', 'https://res.cloudinary.com/dggmk0peo/image/upload/v1757241008/casa-de-hormigon-en-un-acantilado-de-portugal-85a6f2ee-2000x1250_aa20e247_1500x938_sgup5v.png',7),
+    ('IMAGE', 'https://res.cloudinary.com/dggmk0peo/image/upload/v1757240994/450_1000_gqh7tb.png',8),
+    ('IMAGE', 'https://res.cloudinary.com/dggmk0peo/image/upload/v1757240991/01jpg_wgwxm9.png',9),
+    ('IMAGE', 'https://res.cloudinary.com/dggmk0peo/image/upload/v1757240998/20997_pfplab.png',10),
+    ('IMAGE', 'https://res.cloudinary.com/dggmk0peo/image/upload/v1757241002/casa-campo-rustica-jardin-paisajistico-rodeado-naturaleza-generada-ia_24640-93827_fbc2ai.png',11),
+    ('IMAGE', 'https://res.cloudinary.com/dggmk0peo/image/upload/v1757241010/casa-familiar-jardin-colores-primavera_21085-10561_yd2fvo.png',12),
+    ('IMAGE', 'https://res.cloudinary.com/dggmk0peo/image/upload/v1757241018/Fachada-casa-de-dos-pisos-forma-cubo_ataecn.png',13)
+;
